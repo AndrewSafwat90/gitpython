9f73ec6dea2f4043abdbef961e369c6de62785c0
Andrew Safwat <andrew.realme90@gmail.com>
2025-04-01 19:39:51 +0200

    from 1 to 10 done

---
+++ py1.py
@@ -0,0 +1,14 @@
+# multiple
+# comments
+# lines
+
+name = "Andrew"
+age = "35"
+country = "Egypt"
+
+# print("Hello, my name is " + name + "and i am" +
+#       age + "years old and i live in " + country)
+
+print(type(name))
+print(type(age))
+print(type(country))
