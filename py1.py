2ff0871a71624acf0a9403efc25a103786767af1
Andrew Safwat <andrew.realme90@gmail.com>
2025-04-02 10:51:22 +0200

    trying

--- py1.py
+++ py1.py
@@ -9,6 +9,61 @@ country = "Egypt"
 # print("Hello, my name is " + name + "and i am" +
 #       age + "years old and i live in " + country)
 
-print(type(name))
-print(type(age))
-print(type(country))
+# print(type(name))
+# print(type(age))
+# print(type(country))
+
+
+myString = "     I love Python     "
+# print(myString[0])
+# print(myString[1])
+# print(myString[2])
+
+
+# print(myString[2:6])
+
+# print(myString.strip())
+# print(myString.rstrip())
+# print(myString.lstrip())
+# print(myString.rstrip())
+# print(myString.lstrip())
+
+b = 'I love 2d graghics and 3d graphics'
+
+# print(b.title())
+
+# a, b, c = "1", "11", "111"
+
+# print(a.zfill(3))
+# print(b.zfill(3))
+# print(c.zfill(3))
+
+# print(b.split(" ", 2))
+
+# print('I love \n 2d \\ graghics \'and\' 3d \t graphics')
+
+c = "Andrew"
+# print(c.center(10))
+# m = c.center(10, "#")
+# print(m.center(14, "*"))
+
+# print(c.index("w"))
+
+# myStringFive = '''First Second 'Test' "Test" Third'''
+# myStringSix = """First Second "Test" \\ 'Test' Third"""
+# print(myStringFive)
+# print(myStringSix)
+
+# e = '''First
+# Second
+# third'''
+# print(e)
+
+# c = 'andrewsafwat'
+# print(c.isnumeric())
+# print(c.isalpha())
+
+age = 35
+
+print("my age is %.1f" % age)
+print("my age is %.2f" % age)
