94f5737cbc95dbe2ffdf733e768a6904452f0257
Andrew Safwat <andrew.realme90@gmail.com>
2025-04-22 18:23:18 +0200

    from 19 to 20

---
+++ 19-20.py
@@ -0,0 +1,24 @@
+# التكليف 01
+num1 = 1
+num2 = 1.5
+num3 = 1 + 2j
+print(num1, type(num1))
+print(num2, type(num2))
+print(num3, type(num3))
+
+
+# التكليف 02
+num3 = 1 + 2j
+print("the imag part is {}".format(num3.imag))
+print("the real part is {}".format(num3.real))
+# التكليف 03
+
+num = 10
+print("{:.10f}".format(num))
+# التكليف 04
+num = 159.650
+num2Int = int(num)
+
+print(num2Int, type(num2Int))
+
+# التكليف 05
--- try from 19 to .py
+++ try from 19 to .py
@@ -1,20 +1,24 @@
-# x = 5 + 6j
+# # x = 5 + 6j
 
-# print(type(x))
+# # print(type(x))
 
-# print("real is {}".format(x.real))
-# print("imaginary is {}".format(x.imag))
+# # print("real is {}".format(x.real))
+# # print("imaginary is {}".format(x.imag))
 
 
-MyList = ["one", "two", "three", 45, 100.5, True]
-print(MyList)
-# print(len(MyList))
+# MyList = ["one", "two", "three", 45, 100.5, True]
+# print(MyList)
+# # print(len(MyList))
 
-b = [1, 2, 3]
+# b = [1, 2, 3]
 
-# MyList.extend(b[0:2:1])
+# # MyList.extend(b[0:2:1])
+# # print(MyList)
+
+# MyList.extend(b[:2])
+
 # print(MyList)
 
-MyList.extend(b[:2])
-
-print(MyList)
+# a = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# print("The position of Index is :{}".format(a.Index(5)))
+# print(f"The position of Index is :{a.Index(5)}")
