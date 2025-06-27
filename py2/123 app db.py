67576bdccdbf41e936cea36cc6692f27508c9fc7
Andrew Safwat <andrew.realme90@gmail.com>
2025-06-27 21:57:42 +0300

    to vid 125

--- py2/123 app db.py
+++ py2/123 app db.py
@@ -1,3 +1,21 @@
+import sqlite3
+
+db = sqlite3.connect('app123.db')
+cr = db.cursor()
+
+cr.execute("CREATE TABLE if not exists users (user_id INTEGER , name TEXT)")
+cr.execute(
+    "CREATE TABLE if not exists skills (name TEXT, progress INTEGER , user_id INTEGER)")
+
+user_id = 1  # Assuming a single user for simplicity
+
+
+def commit_and_close():
+    db.commit()
+    db.close()
+    print("Database connection closed.")
+
+
 message = """
 what do you want?
 "s" => Show All Skills
@@ -14,23 +32,39 @@ commands_list = ["s", "a", "d", "u", "q"]
 
 
 def show_all_skills():
-    print("Showing all skills...")
+    cr.execute(f"SELECT * FROM skills WHERE user_id = '{user_id}'")
+    result = cr.fetchall()
+    print(f"Total skills: {len(result)}")
+    if len(result) > 0:
+        print("showing all skills with progress: ")
+    for row in result:
+        print(f"Skill: {row[0]}, Progress: {row[1]}%")
+    commit_and_close()
 
 
 def add_new_skill():
-    print("Adding a new skill...")
+    sk = input("Enter Skill name: ").strip().capitalize()
+    progress = input("Enter Skill progress: ").strip()
+    cr.execute(
+        f"INSERT INTO skills (name, progress, user_id) VALUES ('{sk}', '{progress}', '{user_id}')")
+    commit_and_close()
 
 
 def delete_skill():
-    print("Deleting a skill...")
+    sk = input("Enter Skill name: ").strip().capitalize()
+    cr.execute(
+        f"delete from skills where name = '{sk}' and user_id = {user_id}")
+    commit_and_close()
 
 
 def update_skill_progress():
     print("Updating skill progress...")
+    commit_and_close()
 
 
 def quit_app():
     print("Quitting the app...")
+    commit_and_close()
 
 
 if input_value in commands_list:
