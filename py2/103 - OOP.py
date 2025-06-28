bdadfecabcf162710d69215413b912eb140c5387
Andrew Safwat <andrew.realme90@gmail.com>
2025-06-28 15:54:46 +0300

    upload vid 103 from PC

---
+++ py2/103 - OOP.py
@@ -0,0 +1,16 @@
+class Member:
+    def __init__(self,):
+        self.name = "Andrew"
+
+
+# memnber_one = Member()
+# print(memnber_one.__class__)
+
+# Member()
+memnber_one = Member()
+memnber_two = Member()
+memnber_three = Member()
+# print(type(memnber_one))
+print(memnber_one.name)
+print(memnber_two.name)
+print(memnber_three.name)
